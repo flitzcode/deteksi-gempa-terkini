da5c12ed1801fd7531c05aef7bad96a529ffbffa
flitzcode <muchamadfaiz@gmail.com>
2022-07-25 13:29:16 +0700

    dashboard gempa terkini

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def ekstraksi_data():
     """
     Tanggal: 23 Juli 2022
@@ -9,18 +13,33 @@ def ekstraksi_data():
     Dirasakan: Dirasakan (Skala MMI): III Ende, III Maumere, III Larantuka, III Lewoleba, II - III Pulau Kalaotoa
     :return:
     """
-    hasil = dict()
-    hasil['tanggal'] = '23 Juli 2022'
-    hasil['waktu'] = '14:35:46 WIB'
-    hasil['magnitudo'] = '5.7'
-    #hasil['kedalaman'] = '13 Km'
-    hasil['lokasi'] = {'ls': 7.57, 'bt': 122.45}
-    hasil['pusat'] = 'Dirasakan (Skala MMI): III Ende, III Maumere, III Larantuka, III Lewoleba, II - III Pulau Kalaotoa'
+    try:
+        content = requests.get('https://bmkg.go.id')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        #200-299 = berhasil, 300-399 =  redirection, 500-599 = server error, 400-499 = client errors,
+        print(content.text)
+        #soup = BeautifulSoup(content)
+        #print(soup.prettify())
 
-    return hasil
+        hasil = dict()
+        hasil['tanggal'] = '23 Juli 2022'
+        hasil['waktu'] = '14:35:46 WIB'
+        hasil['magnitudo'] = '5.7'
+        #hasil['kedalaman'] = '13 Km'
+        hasil['lokasi'] = {'ls': 7.57, 'bt': 122.45}
+        hasil['pusat'] = 'Dirasakan (Skala MMI): III Ende, III Maumere, III Larantuka, III Lewoleba, II - III Pulau Kalaotoa'
+
+        return hasil
+    else:
+        return None
 
 
 def tampilkan_data(result):
+    if result is None:
+        print('Tidak bisa menemukan data gempa terkini')
+        return
     print('Gempa Terakhir berdasarkan BMKG')
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['tanggal']}")
