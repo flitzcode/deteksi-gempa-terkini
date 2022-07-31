9859f71dfbd3d18e295d475cd62be7ef917d4cae
flitzcode <muchamadfaiz@gmail.com>
2022-07-31 23:00:26 +0700

    Finish real data from BMKG.go.id

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ MODULARISASI DENGAN PACKAGE
 import gempaterkini
 
 if __name__ == '__main__':
-    print('Aplikasi utama')
+    print(f'Aplikasi utama menggunakan package ang memiliki deskripsi {gempaterkini.description}')
     result = gempaterkini.ekstraksi_data()
     gempaterkini.tampilkan_data(result)
