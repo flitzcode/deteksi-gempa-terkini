494dca2fae35e9cd8a61dea51f27e15047edf15b
flitzcode <muchamadfaiz@gmail.com>
2022-08-01 08:28:19 +0700

    Final Version

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ MODULARISASI DENGAN PACKAGE
 import gempaterkini
 
 if __name__ == '__main__':
-    print(f'Aplikasi utama menggunakan package ang memiliki deskripsi {gempaterkini.description}')
-    result = gempaterkini.ekstraksi_data()
-    gempaterkini.tampilkan_data(result)
+    gempa_di_indonesia = gempaterkini.GempaTerkini('https://bmkg.go.id')
+    print(f'Aplikasi utama menggunakan package yang memiliki deskripsi {gempa_di_indonesia.description}')
+    gempa_di_indonesia.tampilkan_keterangan()
+    gempa_di_indonesia.run()
